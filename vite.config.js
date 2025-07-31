420d970796c5ace07f6f8f084d2c389183f2da8b
joe-nontakorn <nontakorn.joe01@gmail.com>
2025-07-31 10:53:41 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
+  base: '/joe-anniversary/', // <== เพิ่มบรรทัดนี้
   server: {
     host: true,       // ✅ เปิดให้เข้าจาก external เช่น ngrok ได้
     port: 5173        // หรือเปลี่ยนพอร์ตตามที่ต้องการ
